b8a3b025adafedaafd19ebaecf59b3851a116c69
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-05-28 02:15:39 +0530

    change bcrypt method usage in helperController to hash async

--- src/controllers/helperController.ts
+++ src/controllers/helperController.ts
@@ -42,7 +42,7 @@ const createHelper = async (req: Request, res: Response) => {
     name,
     email,
     username,
-    password: bcrypt.hashSync(password, 10),
+    password: await bcrypt.hash(password, 10),
   });
 
   if (latitude && longitude) {
