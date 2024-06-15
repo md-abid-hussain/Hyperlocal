4f9a6317112fa607a36b6916fdafca780d9e2378
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-06-15 22:10:00 +0530

    added register controller to auth route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,6 +3,7 @@ import authController from '../controllers/authController';
 
 const router = Router();
 
+router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/refresh', authController.refresh);
 router.post('/logout', authController.logout);
