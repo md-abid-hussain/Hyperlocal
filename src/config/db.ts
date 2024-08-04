a0cecf684c81d7c9eaf39a343584c66769a2bc47
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-08-04 20:38:59 +0530

    added reset and seed function in db config file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,17 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+// import reset from './reset'
+// import seed from './seed'
 
-const dbUrl = process.env.DATABASE_URL as string
+const dbUrl = process.env.DATABASE_URL as string;
 const connectDB = async () => {
   try {
-    await mongoose.connect(dbUrl)
-    console.log('Database connected successfully')
+    await mongoose.connect(dbUrl);
+    console.log('Database connected successfully');
+    // reset()
+    // seed()
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 
-export default connectDB
+export default connectDB;
